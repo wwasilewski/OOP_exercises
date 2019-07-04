7538b7b57b13ec0e67f620343025eed549308274
wwasilewski <w.wasilewski@gmail.com>
2019-07-04 22:28:57 +0200

    adding infinite loop to enum_example2

--- src/main/java/enum_example2/Main.java
+++ src/main/java/enum_example2/Main.java
@@ -14,20 +14,29 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your size (SMALL, MEDIUM, LARGE, EXTRA_LARGE): ");
+        String input = "";
 
-        String input = sc.next().toUpperCase();
+        while (true) {
 
-        //validating user's enum input
-        if (EnumUtils.isValidEnum(Size.class, input)) {
-            Size size = Enum.valueOf(Size.class, input);
-            System.out.println("Your size: " + size);
-            System.out.println("Your size's abbreviation: " + size.getAbbreviation());
-            System.out.println("Your size in polish: " + size.getLanguagePL());
-            if (size == Size.EXTRA_LARGE) {
-                System.out.println("Good job, you didn't miss \"_\" in EXTRA_LARGE");
+            try {
+                input = sc.nextLine().toUpperCase();
+
+                //validating user's enum input
+                if (EnumUtils.isValidEnum(Size.class, input)) {
+                    Size size = Enum.valueOf(Size.class, input);
+                    System.out.println("Your size: " + size);
+                    System.out.println("Your size's abbreviation: " + size.getAbbreviation());
+                    System.out.println("Your size in polish: " + size.getLanguagePL());
+                    if (size == Size.EXTRA_LARGE) {
+                        System.out.println("Good job, you didn't miss \"_\" in EXTRA_LARGE");
+                    }
+                    break;
+                } else {
+                    System.out.println("Invalid data, use SMALL, MEDIUM, LARGE OR EXTRA_LARGE only.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } else {
-            System.out.println("Invalid size, use SMALL, MEDIUM, LARGE OR EXTRA_LARGE only.");
         }
         sc.close();
     }
